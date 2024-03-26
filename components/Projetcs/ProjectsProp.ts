ea89ce3f29d5487df1b4ef4ec6524ee49b26a6ce
Eduardo Scrobote <EduardoScroboteProfissional@gmail.com>
2024-03-26 09:33:43 -0300

    fix: projects-dates

--- components/Projetcs/ProjectsProp.ts
+++ components/Projetcs/ProjectsProp.ts
@@ -2,19 +2,19 @@ import { ProjectPropType } from "./Projetcs.type";
 
 export const ProjectsData: ProjectPropType = [
   {
-    date: "2023 / atualmente",
+    date: "2023 / 02/2024",
     project: "Programação fortificada",
     desc: "Projeto de criação de conteúdo sobre desenvolvimento de software",
     path: "https://www.youtube.com/@ProgramacaoFortificada/featured",
   },
   {
-    date: "2023 / atualmente",
+    date: "2023 / 02/2024",
     project: "Agrigooders",
     desc: "Criação da plataforma agrigooders e painel de administração da agrigoldens junto as equipes da Appsentinel, Camponho Digital e Orbia",
     path: "https://agrigooders.com.br/",
   },
   {
-    date: "2023 / atualmente",
+    date: "2023 / 03/24",
     project: "App-Sentinel",
     desc: "Projeto de criação de conteúdo sobre desenvolvimento de software",
   },
