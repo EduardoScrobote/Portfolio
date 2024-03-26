1d874aeb667aeb9bcab1f516b333e063982eb542
Eduardo Scrobote <EduardoScroboteProfissional@gmail.com>
2024-03-26 09:22:37 -0300

    feat: add-bravery´s-academy-project

--- components/Projetcs/ProjectsProp.ts
+++ components/Projetcs/ProjectsProp.ts
@@ -18,4 +18,10 @@ export const ProjectsData: ProjectPropType = [
     project: "App-Sentinel",
     desc: "Projeto de criação de conteúdo sobre desenvolvimento de software",
   },
+  {
+    date: "2024 / atualmente",
+    project: "Bravery´s Academy",
+    desc: "Projeto para um professor de dota 2 com mais de 150 alunos",
+    path: "https://bravery-black.vercel.app/",
+  },
 ];
